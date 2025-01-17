175623b54655900c87bf85b141c040f5af9b2132
joaolago <lago.joao@discente.ufma.br>
2025-01-17 18:09:48 -0300

    /logo-ufma

--- front/src/layout/TelaApresentação/HeaderApresentaçao.tsx
+++ front/src/layout/TelaApresentação/HeaderApresentaçao.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import { BotaoInstrucoes,  BotaoDados, BotaoMapeamento } from '../../components/Buttons';
+import { BotaoInstrucoes, BotaoDados, BotaoMapeamento } from '../../components/Buttons';
+import LogoUFMA from '../../components/LogoUFMA.png'; // Verifique se o caminho está correto
 
 const HeaderApresentaçao: React.FC = () => {
   return (
-     <>
-      <h1 className="text-center text-white mt-8 text-4xl">
-        APRESENTAÇÃO
-      </h1>
-      <div className="w-full bg-gray-400 h-12 mt-5 flex items-center justify-center">
+    <header className="bg-gray-800 fixed w-full top-0 left-0 flex flex-col">
+      
+      {/* Container para Logo e Título com Padding */}
+      <div className="w-full p-4 flex items-center">
+        <img src={LogoUFMA} alt="UFMA Logo" className="w-20 h-20" />
+        <h1 className="text-white justify-center text-4xl ml-4">APRESENTAÇÃO</h1>
+      </div>
+      
+      {/* Container para os Botões ocupando toda a largura */}
+      <div className="w-full bg-gray-400 h-12 flex items-center justify-center">
         <div className="flex space-x-4">
-           <BotaoMapeamento/>
-           < BotaoInstrucoes />
-           <BotaoDados />
-           
-         </div>
-       </div>
-     </>
-   );
- };
+          <BotaoMapeamento />
+          <BotaoInstrucoes />
+          <BotaoDados />
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default HeaderApresentaçao;
--- front/src/layout/TelaApresentação/MainSectionApresentaçao.tsx
+++ front/src/layout/TelaApresentação/MainSectionApresentaçao.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainSectionApresentaçao: React.FC = () => {
   return (
-    <div className="bg-gray-400 rounded-lg w-[1000px] h-[400px] p-8 text-3xl overflow-auto fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <div className="bg-gray-400 rounded-lg w-[1000px] h-[400px] p-10 text-3xl overflow-auto fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <p> APRESENTAÇÃO Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed ad soluta distinctio optio ut facere 
       blanditiis totam in nemo quasi, tempore sit delectus, mollitia, perferendis veniam ea atque assumenda modi? Lorem 
       ipsum dolor sit amet consectetur adipisicing elit. Doloribus minima impedit incidunt assumenda labore omnis sapiente
