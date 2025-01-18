4f82c137267e487faf8939329b62309a51a1fbe4
joaolago <lago.joao@discente.ufma.br>
2025-01-17 21:18:24 -0300

    texto-apresentaçao

--- front/src/layout/TelaDados/MainSectionDados.tsx
+++ front/src/layout/TelaDados/MainSectionDados.tsx
@@ -3,15 +3,9 @@ import React from 'react';
 
 const MainSectionDados: React.FC = () => {
   return (
-    <div className="
-      bg-gray-400  rounded-lg  w-full  md:w-96  h-64 md:h-96 flex items-center justify-center text-3xl  overflow-auto absolute top-1/2 
-      left-1/2 
-      transform 
-      -translate-x-1/2 
-      -translate-y-1/2 
-      mt-24
-    ">
-      DADOS
+    <div className="bg-gray-400 text-justify rounded-lg w-[1300px] h-[550px] p-10 text-2xl overflow-auto fixed bottom-10 left-1/2 transform -translate-x-1/2 space-y-6">
+  
+      m
     </div>
   );
 };
